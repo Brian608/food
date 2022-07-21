716c0ee2b1ba9976d681c61f381d08bd27669040
feather <Brian608@163.com>
2022-07-21 08:33:40 +0800

    依赖配置

---
+++ food-api/src/main/java/org/feather/food/FoodApplication.java
@@ -0,0 +1,21 @@
+package org.feather.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: food
+ * @package: org.feather.food
+ * @className: FoodApplication
+ * @author: feather(杜雪松)
+ * @description: TODO
+ * @since: 2022/7/21 08:08
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class FoodApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodApplication.class,args);
+    }
+
+}
